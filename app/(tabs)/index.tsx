302bdb2f69b050e399e8f52a46a1c9f2b96f753b
om <omrajesh9203@gmail.com>
2026-03-18 23:46:39 -0700

    Removed

--- app/(tabs)/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import MainDashboard from '../../src/features/sos-tracking/components/MainDashboard';
-
-export default function Index() {
-  return <MainDashboard />;
-}
